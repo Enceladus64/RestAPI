e8fba9475b89dc6c77fc9c84c60d803b53a445f1
roman <roman>
2017-07-19 20:19:07 +0200

    init commit

---
+++ RestClientScratch/src/main/java/de/romankoutny/rest/client/Main.java
@@ -0,0 +1,11 @@
+package de.romankoutny.rest.client;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
